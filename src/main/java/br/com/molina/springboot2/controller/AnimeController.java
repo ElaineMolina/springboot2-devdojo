89c3d24f21c27d2b424ff95366e9477cbccf6ef6
ElaineMolina <elainemolina10@gmail.com>
2022-01-07 18:00:08 -0300

    Método GET

--- src/main/java/br/com/molina/springboot2/controller/AnimeController.java
+++ src/main/java/br/com/molina/springboot2/controller/AnimeController.java
@@ -6,6 +6,8 @@ import br.com.molina.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +24,8 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list() {
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
     }
 }
