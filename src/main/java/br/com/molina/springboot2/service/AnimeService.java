83c58ceb3ba3eb19a5bbe36af9105ad6376cbeac
ElaineMolina <elainemolina10@gmail.com>
2022-01-10 12:27:59 -0300

    Método GET 2

--- src/main/java/br/com/molina/springboot2/service/AnimeService.java
+++ src/main/java/br/com/molina/springboot2/service/AnimeService.java
@@ -1,14 +1,38 @@
 package br.com.molina.springboot2.service;
 
 import br.com.molina.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
+
+    public static List<Anime> animes;
+    static{
+    animes = new ArrayList<>(List.of(new Anime(1l,"Boku No Hero"),
+                                     new Anime(2l, "Berserk")));
+    }
     //private final AnimeRepository animeRepository
     public List<Anime> listAll(){
-        return List.of(new Anime(1L,"Boku No Hero"), new Anime(2L,"Berserk"));
+        return  animes;
+    }
+
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "Animes not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3,100000));
+        animes.add(anime);
+        return anime;
     }
 }
