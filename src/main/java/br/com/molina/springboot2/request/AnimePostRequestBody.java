27d426afaca5537b81f1f56935f1d21f2431aa79
ElaineMolina <elainemolina10@gmail.com>
2022-01-19 16:55:28 -0300

    20 - Validação de campos

--- src/main/java/br/com/molina/springboot2/request/AnimePostRequestBody.java
+++ src/main/java/br/com/molina/springboot2/request/AnimePostRequestBody.java
@@ -2,9 +2,11 @@ package br.com.molina.springboot2.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
-
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 
 }
