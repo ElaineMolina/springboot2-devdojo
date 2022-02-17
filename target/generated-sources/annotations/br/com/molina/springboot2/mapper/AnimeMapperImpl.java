02731f61cacb93a152046e51f41ac5cc2363ee26
ElaineMolina <elainemolina10@gmail.com>
2022-02-17 18:38:18 -0300

    49 - Monitorando aplicação com prometheus

--- target/generated-sources/annotations/br/com/molina/springboot2/mapper/AnimeMapperImpl.java
+++ target/generated-sources/annotations/br/com/molina/springboot2/mapper/AnimeMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-02-17T16:08:33-0300",
+    date = "2022-02-17T17:12:52-0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
